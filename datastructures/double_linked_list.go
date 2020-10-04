4c595e203829a28db4096017c5b4f493cba6995c
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-10-04 17:58:52 +0200

    feat(Data Structure): Double Linked List

---
+++ datastructures/double_linked_list.go
@@ -0,0 +1,88 @@
+package datastructures
+
+package datastructures
+
+type DoubleLikedList struct {
+	head *node
+	tail *node
+	size int
+}
+
+func (list *DoubleLikedList) InsertFront(value interface{}) {
+	node := &node{value, list.Head(), nil}
+	if list.head == nil {
+		list.head = node
+		list.tail = node
+	} else {
+		node.next = list.head
+		list.head.prev = node
+		list.head = node
+	}
+
+	list.size++
+}
+
+func (list *DoubleLikedList) InsertBack(value interface{}) {
+	node := &node{value, nil, list.tail}
+	if list.head == nil {
+		list.head = node
+	} else {
+		node.prev = list.tail
+		list.tail.next = node
+	}
+	list.tail = node
+
+	list.size++
+}
+
+func (list *DoubleLikedList) RemoveFront() interface{} {
+	if list.head == list.tail {
+		node := list.head
+		list.head = nil
+		list.tail = nil
+		return node.value
+	}
+
+	node := list.head
+	list.head = node.next
+	list.head.prev = nil
+	list.size--
+	return node.value
+}
+
+func (list *DoubleLikedList) RemoveBack() interface{} {
+	if list.head == list.tail {
+		node := list.head
+		list.head = nil
+		list.tail = nil
+		return node.value
+	}
+
+	node := list.tail
+	node.prev.next = nil
+	list.tail = node.prev
+	list.size--
+	return node.value
+}
+
+func (list *DoubleLikedList) Head() *node {
+	return list.head
+}
+
+func (list *DoubleLikedList) Tail() *node {
+	return list.tail
+}
+
+func (list *DoubleLikedList) Empty() bool {
+	return list.size == 0
+}
+
+func (list *DoubleLikedList) Size() int {
+	return list.size
+}
+
+type node struct {
+	value interface{}
+	next  *node
+	prev  *node
+}
