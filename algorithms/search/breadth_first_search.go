2a1524d535d4a2e0e8d43a98f627be9067c2f22e
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-10-19 16:37:05 +0200

    feat(Algorithm): add ShortestPath BFT

--- algorithms/search/breadth_first_search.go
+++ algorithms/search/breadth_first_search.go
@@ -1,1 +1,32 @@
 package search
+
+import (
+	"galgo/datastructures"
+	"math"
+)
+
+func ShortestPath(adj [][]int, start int) []int {
+	var shortestPaths = make([]int, len(adj))
+	var isVisited = make([]bool, len(adj))
+	for i := 0; i < len(shortestPaths); i++ {
+		shortestPaths[i] = math.MaxInt64
+	}
+	var queue = datastructures.NewQueue()
+	queue.Enqueue(start)
+	isVisited[start] = true
+	shortestPaths[start] = 0
+
+	for !queue.Empty() {
+		var edge = queue.Dequeue().(int)
+		for i := 0; i < len(adj[edge]); i++ {
+			y := adj[edge][i]
+			if !isVisited[y] {
+				isVisited[y] = true
+				shortestPaths[y] = shortestPaths[edge] + 1
+				queue.Enqueue(y)
+			}
+		}
+	}
+
+	return shortestPaths
+}
