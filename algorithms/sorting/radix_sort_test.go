4b22805071b7c6e2daf497b878e97445b9b8b2fe
Alex Prut <1648497+alexprut@users.noreply.github.com>
2020-11-14 10:58:24 +0100

    test: Radix Sort

--- algorithms/sorting/radix_sort_test.go
+++ algorithms/sorting/radix_sort_test.go
@@ -1,1 +1,40 @@
 package sorting
+
+import "testing"
+
+func TestRadixSort(t *testing.T) {
+	toSort := []int {5, 2, 1, 9, 0, 33, 3, 3, 0}
+	expected := []int {0, 0, 1, 2, 3, 3, 5, 9, 33}
+
+	RadixSort(toSort, 2)
+
+	AssertIntArrayEquals(t, expected, toSort)
+}
+
+func TestShouldSortInverseSorted(t *testing.T) {
+	toSort := []int {33, 9, 5, 3, 3, 2, 1, 0, 0}
+	expected := []int {0, 0, 1, 2, 3, 3, 5, 9, 33}
+
+	RadixSort(toSort, 2)
+
+	AssertIntArrayEquals(t, expected, toSort)
+}
+
+func TestShouldSortSorted(t *testing.T) {
+	toSort := []int {0, 0, 1, 2, 3, 3, 5, 9, 33}
+	expected := []int {0, 0, 1, 2, 3, 3, 5, 9, 33}
+
+	RadixSort(toSort, 2)
+
+	AssertIntArrayEquals(t, expected, toSort)
+}
+
+func TestShouldHandleEdgeCases(t *testing.T) {
+	empty := []int {}
+	RadixSort(empty, 1)
+	single := []int {1}
+	RadixSort(single, 1)
+
+	AssertIntArrayEquals(t, []int {}, empty)
+	AssertIntArrayEquals(t, []int {1}, single)
+}
